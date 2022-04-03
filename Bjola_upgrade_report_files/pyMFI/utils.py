2caef37a2c75b7065a67cdfa1c2256d191279b45
msalvalaglio <matteo.salvalaglio@gmail.com>
2022-04-03 16:36:43 +0100

    Work on the MFI notebooks

--- Bjola_upgrade_report_files/pyMFI/utils.py
+++ Bjola_upgrade_report_files/pyMFI/utils.py
@@ -10,27 +10,83 @@ from labellines import labelLine
 import scipy.io
 import matplotlib as mpl
 
+#Load the HILLS data
+def load_HILLS_1D(hills_name = "HILLS"):
+    for file in glob.glob(hills_name):
+        hills = np.loadtxt(file)
+        hills = hills[:-1]
+        hills0 = hills[0]
+        hills0[3] = 0
+        hills = np.concatenate(([hills0],hills))
+    return hills
 
+#Load the trajectory (position) data
+def load_position_1D(position_name = "position"):
+    for file1 in glob.glob(position_name):
+        colvar = np.loadtxt(file1)
+    return colvar[:-1, 1]
 
-def run_langevin1D(length, sigma=0.1, height=0.1, biasfactor=10):
-    with open("plumed.dat","w") as f:
-        print("""p: DISTANCE ATOMS=1,2 COMPONENTS
-ff: MATHEVAL ARG=p.x PERIODIC=NO FUNC=(7*x^4-23*x^2)
-bb: BIASVALUE ARG=ff
-METAD ARG=p.x PACE=100 SIGMA={} HEIGHT={} GRID_MIN=-3 GRID_MAX=3 GRID_BIN=200 BIASFACTOR={} TEMP=120
-PRINT FILE=position ARG=p.x STRIDE=10""".format(sigma,height, biasfactor),file=f)
+### Algorithm to run 1D MFI
+def MFI_1D_simple():
+    #initialise force terms
+    Fbias = np.zeros(len(x));
+    Ftot_num = np.zeros(len(x));
+    Ftot_den = np.zeros(len(x));
 
-    with open("input","w") as f:
-        print("""temperature 1
-tstep 0.005
-friction 1
-dimension 1
-nstep {}
-ipos -1.0
-periodic false""".format(length),file=f)
-    
-    #Start WT-Metadynamic simulation
-    !plumed pesmd < input
+    for i in range(total_number_of_hills):
+        # Build metadynamics potential
+        s = HILLS[i, 1]  # center position of gausian
+        sigma_meta2 = HILLS[i, 2] ** 2  # width of gausian
+        gamma = HILLS[i, 4]  #scaling factor of gausian
+        height_meta = HILLS[i, 3] * ((gamma - 1) / (gamma))  # Height of Gausian
+        kernelmeta = np.exp(-0.5 * (((x - s) ** 2) / (sigma_meta2)))
+        Fbias = Fbias + height_meta * kernelmeta * ((x - s) / (sigma_meta2)) #Bias force due to Metadynamics potentials
+
+        # Estimate the biased proabability density
+        pb_t = np.zeros(len(x))
+        Fpbt = np.zeros(len(x))
+        data = position[i * stride: (i + 1) * stride] #positons of window of constant bias force.
+        for j in range(stride):
+            kernel = const * np.exp(- (x - data[j])**2 / (2*bw2) ) #probability density of 1 datapoint
+            pb_t = pb_t + kernel #probability density of window 
+            Fpbt = Fpbt + kT * kernel * (x - data[j]) / bw2
+
+        # Estimate of the Mean Force
+        Ftot_den = Ftot_den + pb_t   #total probability density
+        dfds = np.divide(Fpbt, pb_t, out=np.zeros_like(Fpbt), where=pb_t != 0) + Fbias
+        Ftot_num = Ftot_num + pb_t * dfds
+        Ftot = np.divide(Ftot_num, Ftot_den, out=np.zeros_like(Ftot_num), where=Ftot_den != 0) #total force
+
+        if (i+1) % (total_number_of_hills/10) == 0:
+            print(str(i+1) + " / " + str(total_number_of_hills))
+            
+    return [Ftot_den, Ftot]
+
+        
+# Integrate Ftot, obtain FES 
+def intg_1D(F):
+    fes = []
+    for j in range(len(x)): fes.append(integrate.simps(F[:j + 1], x[:j + 1]))
+    fes = fes - min(fes)
+    return fes
+
+# Calculate average deviation of 1D FES
+def error_1D(FES):
+    AD = abs(FES - y)
+    AAD = sum(AD) / len(AD)
+    print("The AAD of the FES is: " + str(AAD))
+    return (AD, AAD)
+
+#define indexing
+def index(position, min_grid=min_x, _grid_space=grid_space):
+    return int((position-min_grid)//_grid_space) + 1
+
+# Calculate average deviation of 1D FES in central region [range_min, range_max]. e.g. [-1.75, 1.75]
+def error_1D_centre(FES, range_min, range_max):
+    AD = abs(FES[index(-1.75):index(1.75)+1] - y[index(-1.75):index(1.75)+1])
+    AAD = sum(AD) / len(AD)
+    print("The AAD of the FES from x=" + str(range_min) +" to x=" + str(range_max) + " is: " + str(AAD))
+    return (AD, AAD)
 
 #Load the HILLS data in 1D
 def load_HILLS_1D(hills_name = "HILLS"):
