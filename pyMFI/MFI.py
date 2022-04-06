1da2c6d12823b279366fea59e8306315061bbbfb
msalvalaglio <matteo.salvalaglio@gmail.com>
2022-04-06 12:25:36 +0100

    Added new example for patching, with Invernizzi potential

--- pyMFI/MFI.py
+++ pyMFI/MFI.py
@@ -271,13 +271,13 @@ def plot_recap_2D(X, Y, FES, TOTAL_DENSITY, CONVMAP, CONV_history):
         CONV_history (_type_): _description_
     """
     fig, axs = plt.subplots(1,4,figsize=(18,3))
-    cp=axs[0].contourf(X,Y,FES,levels=range(0,50,1),cmap='coolwarm',antialiased=False,alpha=0.8);
+    cp=axs[0].contourf(X,Y,FES,levels=range(0,50,1),cmap='YlGnBu_r',antialiased=False,alpha=0.8);
     cbar = plt.colorbar(cp, ax=axs[0])
     axs[0].set_ylabel('CV2',fontsize=11)
     axs[0].set_xlabel('CV1',fontsize=11)
     axs[0].set_title('Free Energy Surface',fontsize=11)
     
-    cp=axs[1].contourf(X,Y,CONVMAP,levels=range(0,40,1),cmap='coolwarm',antialiased=False,alpha=0.8);
+    cp=axs[1].contourf(X,Y,CONVMAP,levels=range(0,40,1),cmap='YlGnBu_r',antialiased=False,alpha=0.8);
     cbar = plt.colorbar(cp, ax=axs[1])
     axs[1].set_ylabel('CV2',fontsize=11)
     axs[1].set_xlabel('CV1',fontsize=11)
@@ -324,7 +324,7 @@ def plot_patch_2D(X, Y, FES, TOTAL_DENSITY):
         CONV_history (_type_): _description_
     """
     fig, axs = plt.subplots(1,2,figsize=(9,3.5))
-    cp=axs[0].contourf(X,Y,FES,levels=range(0,50,1),cmap='coolwarm',antialiased=False,alpha=0.8);
+    cp=axs[0].contourf(X,Y,FES,levels=range(0,10,1),cmap='YlGnBu_r',antialiased=False,alpha=0.8);
     cbar = plt.colorbar(cp, ax=axs[0])
     axs[0].set_ylabel('CV2',fontsize=11)
     axs[0].set_xlabel('CV1',fontsize=11)
--- pyMFI/langevin.py
+++ pyMFI/langevin.py
@@ -18,3 +18,21 @@ nstep {}
 ipos {},{}
 periodic false""".format(nsteps,ipos[0],ipos[1]),file=f)
     
+
+
+def run_2D_Invernizzi(pace=200, nsteps=100000, sigma=0.1, height=0.5, biasfactor=10, ipos=np.array([-1,-1]),tag=1):
+    with open("plumed.dat","w") as f:
+        print("""p: DISTANCE ATOMS=1,2 COMPONENTS
+ff: MATHEVAL ARG=p.x,p.y PERIODIC=NO FUNC=(1.34549*x^4+1.90211*x^3*y+3.92705*x^2*y^2-6.44246*x^2-1.90211*x*y^3+5.58721*x*y+1.33481*x+1.34549*y^4-5.55754*y^2+0.904586*y+18.5598)
+bb: BIASVALUE ARG=ff
+METAD ARG=p.x,p.y PACE={} SIGMA={},{} HEIGHT={} GRID_MIN=-3,-3 GRID_MAX=3,3 GRID_BIN=300,300 BIASFACTOR={} TEMP=120 FILE=HILLSinve_{}
+PRINT FILE=positioninve_{} ARG=p.x,p.y STRIDE=10""".format(pace, sigma, sigma, height, biasfactor,tag,tag),file=f)
+
+    with open("input","w") as f:
+        print("""temperature 1
+tstep 0.005
+friction 10
+dimension 2
+nstep {}
+ipos {},{}
+periodic false""".format(nsteps,ipos[0],ipos[1]),file=f)
